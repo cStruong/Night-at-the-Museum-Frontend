ab94417fa117ca9bdf96af0749cbc1794c73137a
cstruong <christophertruongm@gmail.com>
2019-04-16 10:10:48 -0400

    images and bg movements

--- index.js
+++ index.js
@@ -1,18 +1,97 @@
-let x = 35;
-let y = 365;
+
+let robberX = 35;
+let robberY = 450;
+let guardX = 800;
+let guardY = 450;
 //characters speed
-let speed = 5;
+let robberSpeed = 5;
+let hiddenStatus = false;
+
+let standSprite;
+let laydownSprite;
+let walkSprite; 
+let stealSprite;
+let sleepSprite;
+let awakeSprite;
+let bg;
+
+function preload() {
+  standSprite = loadImage('assets/stand.png');
+  laydownSprite = loadImage('assets/laydown.png');
+  stealSprite = loadImage('assets/steal.png');
+  sleepSprite = loadImage('assets/sleeping.png');
+  awakeSprite = loadImage('assets/awake.png');
+  walkSprite = loadAnimation('assets/walk1.png','assets/walk2.png', 'assets/walk3.png');
+  revWalkSprite = loadAnimation('assets/revwalk1.png', 'assets/revwalk2.png', 'assets/revwalk3.png');
+  bg = loadImage('assets/bg.jpg');
+}
 
 function setup() {
-  createCanvas(1425, 400);
+  createCanvas(920, 640);
+
+}
+
+
+function draw(){
+  background(bg);
+
+  let isAsleep = false;
+  if (isAsleep === true) {
+    image(sleepSprite, guardX, guardY)
+  } else if (isAsleep === false) {
+    image(awakeSprite, guardX, guardY)
+  }
+
+  if (keyIsDown(LEFT_ARROW)) {
+    robberX = robberX - robberSpeed;
+    let tempY = robberY + 40
+    let tempX = robberX
+    animation(revWalkSprite, tempX, tempY);
+  } else if (keyIsDown(RIGHT_ARROW)) {
+    robberX = robberX + robberSpeed;
+    let tempX = robberX + 40
+    let tempY = robberY + 40
+    animation(walkSprite, tempX, tempY);
+  } else if (keyIsDown(DOWN_ARROW)) {
+    image(laydownSprite, robberX, robberY);
+  } else if (keyIsPressed === false) {
+    image(standSprite, robberX, robberY);
+  } else if (keyIsDown(UP_ARROW)) {
+    image(stealSprite, robberX, robberY);
+  } else {
+    image(standSprite, robberX, robberY);
+  }
+
+
+
+}
+
+/*
+let guardVisionX = 1390;
+let guardVisionY = 300;
+let guardVisionSpeed = 2;
+
+function setup() {
+ createCanvas(1425, 400);
 }
 function draw(){
-  if (keyIsDown(LEFT_ARROW)) {
-    x = x - speed;
-  }
-  else if (keyIsDown(RIGHT_ARROW)) {
-    x = x + speed;
-  }
-  clear();
-  ellipse(x, y, 50, 50);
+ background('gray');
+ if (keyIsDown(LEFT_ARROW)) {
+   robberX = robberX - robberSpeed;
+ }
+ else if (keyIsDown(RIGHT_ARROW)) {
+   robberX = robberX + robberSpeed;
+ }
+ else if (keyIsDown(DOWN_ARROW)) {
+   var i;
+   for (i = 0; i < 25; i++){
+     guardVisionX = guardVisionX - robberSpeed;
+   }
+   hiddenStatus = true;
+ }
+ hiddenStatus = false;
+ let robber = ellipse(robberX, robberY, 50, 50);
+ let guard = ellipse(guardX, guardY, 50, 50);
+ let guardVision = ellipse(guardVisionX, guardVisionY, 20, 5);
 }
+*/
